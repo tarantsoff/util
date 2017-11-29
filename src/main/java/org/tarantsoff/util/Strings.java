910384997539d26ac268f80f045dad3275528abb
Andrii <tarantsoff@gmail.com>
2017-11-30 01:35:03 +0200

    Initial commit

---
+++ src/main/java/org/tarantsoff/util/Strings.java
@@ -0,0 +1,45 @@
+package org.tarantsoff.util;
+
+public class Strings {
+
+    public static int lastIndexOf(String source, String searchFor){
+
+
+        if (source.length() < searchFor.length()) {
+            return -1;
+        }
+
+        char[] sourceChars = source.toCharArray();
+        char[] searchForChars = searchFor.toCharArray();
+
+        for (int i = sourceChars.length; i > searchFor.length(); i--) {
+/*
+            if (sourceChars[i] != searchForChars[i]) {
+                break; // go to the next character
+            }
+            // if found - compare sub array
+*/
+
+
+        }
+
+        return -1;
+    }
+
+    public static boolean arraysContains(char[] src, int srcStartIndex, char[] dst) {
+        return arraysContains(src, srcStartIndex, dst, 0, dst.length);
+    }
+
+    public static boolean arraysContains(char[] src, int startIndex, char[] dst, int dstStartIndex, int length) {
+        for (int i = 0; i < length; i++) {
+            System.out.println(src[i + startIndex] +
+                    " != " +
+                    dst[i + dstStartIndex] +
+                    " - >" + (src[i + startIndex] != dst[i + dstStartIndex]));
+            if (src[i + startIndex] != dst[i + dstStartIndex]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/org/tarantsoff/util/StringsTest.java
@@ -0,0 +1,74 @@
+package org.tarantsoff.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringsTest {
+
+    @Test
+    public void testLastIndexOfSameChar(){
+        int result = Strings.lastIndexOf("c", "c");
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void testLastIndexOfDifferentChar(){
+        int result = Strings.lastIndexOf("s", "c");
+        Assertions.assertEquals(-1, result);
+    }
+
+    @Test
+    public void testLastIndexOfEmptyString(){
+        int result = Strings.lastIndexOf("", "");
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void testLastIndexOfSameString(){
+        int result = Strings.lastIndexOf("string", "string");
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void compareArraysTestEquals(){
+        char[] src = "string".toCharArray();
+        char[] dst = "string".toCharArray();
+        Assertions.assertTrue(Strings.arraysContains(src, 0, dst));
+    }
+
+    @Test
+    public void compareArraysTestEqualsLonger(){
+        char[] src = "string1".toCharArray();
+        char[] dst = "string".toCharArray();
+        Assertions.assertTrue(Strings.arraysContains(src, 0, dst));
+    }
+
+    @Test
+    public void compareArraysTestEqualsLonger1(){
+        char[] src = "123string".toCharArray();
+        char[] dst = "string".toCharArray();
+        Assertions.assertTrue(Strings.arraysContains(src, 3, dst));
+    }
+
+    @Test
+    public void compareArraysTestEqualsLonger4(){
+        char[] src = "1234string".toCharArray();
+        char[] dst = "string".toCharArray();
+        Assertions.assertFalse(Strings.arraysContains(src, 3, dst));
+    }
+
+    @Test
+    public void compareArraysTestEqualsLonger5(){
+        char[] src = "1234string".toCharArray();
+        char[] dst = "string".toCharArray();
+        Assertions.assertTrue(Strings.arraysContains(src, 4, dst, 0, 6));
+    }
+
+    @Test
+    public void compareArraysTestEqualsLonger7(){
+        char[] src = "1234str4321".toCharArray();
+        char[] dst = "1234555str5".toCharArray();
+        Assertions.assertTrue(Strings.arraysContains(src, 4, dst, 7, 3));
+    }
+
+}
